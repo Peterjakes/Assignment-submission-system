65ed13ad7c845c6ed7cb8069d5350ac9b4794521
jakes <kiberu@gmail.com>
2025-04-25 14:35:27 +0300

    added Assignment model schema

---
+++ backend/models/Assignment.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+
+const assignmentSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    published: {
+      type: Boolean,
+      default: false,
+    },
+    totalMarks: {
+      type: Number,
+      required: true,
+      default: 100,
+    },
+  },
+  { timestamps: true },
+)
+
+module.exports = mongoose.model("Assignment", assignmentSchema)
