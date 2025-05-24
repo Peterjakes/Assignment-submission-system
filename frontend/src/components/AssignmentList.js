d17e20e71c606313a56072c2e4fe75fc4a92cabf
jakes <kiberu@gmail.com>
2025-05-24 23:12:32 +0300

    created basic AssignmentList component

---
+++ frontend/src/components/AssignmentList.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Card } from "react-bootstrap"
+
+const AssignmentList = () => {
+  return (
+    <Card>
+      <Card.Header>
+        <Card.Title>Assignments</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <p>Assignment list will go here</p>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default AssignmentList
