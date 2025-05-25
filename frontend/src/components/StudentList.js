96ad0317036ed760a5aa6288079d721f2463c8d1
jakes <kiberu@gmail.com>
2025-05-25 11:21:46 +0300

    created basic StudentList component

---
+++ frontend/src/components/StudentList.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Card } from "react-bootstrap"
+
+const StudentList = () => {
+  return (
+    <Card>
+      <Card.Header>
+        <Card.Title>Student List</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <p>Student list will be displayed here</p>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default StudentList
