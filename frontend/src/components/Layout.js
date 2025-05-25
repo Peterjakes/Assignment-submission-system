3bad4336cb6810bdbd54c24d1e9c4d190bdcbec5
jakes <kiberu@gmail.com>
2025-05-25 22:55:41 +0300

    added navigation links to dashboard and assignments pages

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,15 +1,28 @@
-import { Outlet } from "react-router-dom"
-import { Navbar, Container } from "react-bootstrap"
+import { Outlet, NavLink } from "react-router-dom"
+import { Navbar, Container, Nav } from "react-bootstrap"
 
 const Layout = () => {
   return (
     <div>
-      <Navbar bg="primary" variant="dark">
+      <Navbar bg="primary" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand>Assignment System</Navbar.Brand>
+          <Navbar.Brand as={NavLink} to="/dashboard">
+            Assignment System
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={NavLink} to="/dashboard">
+                Dashboard
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/assignments">
+                Assignments
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-      
+
       <Container>
         <Outlet />
       </Container>
