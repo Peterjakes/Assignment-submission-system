f396752abd742f49f61ec68184c2980d778c10e8
jakes <kiberu@gmail.com>
2025-05-25 23:35:09 +0300

    added complete admin routing for student management and user management functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,12 @@ import Layout from "./components/Layout"
 import Login from "./components/Login"
 import Register from "./components/Register"
 import Dashboard from "./components/Dashboard"
+import StudentList from "./components/StudentList"
+import AddStudentPage from "./components/AddStudentPage"
+import UpdateStudentPage from "./components/UpdateStudentPage"
 import AssignmentList from "./components/AssignmentList"
 import CreateAssignment from "./components/CreateAssignment"
+import UserManagement from "./components/UserManagement"
 import "./App.css"
 
 function App() {
@@ -18,7 +22,6 @@ function App() {
       <BrowserRouter>
         <ToastContainer />
         <Routes>
-          {/* Public routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
@@ -31,10 +34,17 @@ function App() {
               {/* Assignment Management */}
               <Route path="/assignments" element={<AssignmentList />} />
               <Route path="/create-assignment" element={<CreateAssignment />} />
+
+              {/* Student Management (Admin only) */}
+              <Route path="/students" element={<StudentList />} />
+              <Route path="/add-student" element={<AddStudentPage />} />
+              <Route path="/update-student/:id" element={<UpdateStudentPage />} />
+
+              {/* User Management (Admin only) */}
+              <Route path="/user-management" element={<UserManagement />} />
             </Route>
           </Route>
 
-          {/* Default redirects */}
           <Route path="/" element={<Navigate to="/dashboard" />} />
           <Route path="*" element={<Navigate to="/dashboard" />} />
         </Routes>
