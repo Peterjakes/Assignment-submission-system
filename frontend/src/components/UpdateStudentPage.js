f9093f1bffefcff616ee5582e2501ca9dc2cbf00
jakes <kiberu@gmail.com>
2025-05-25 11:46:53 +0300

    created basic UpdateStudentPage component

---
+++ frontend/src/components/UpdateStudentPage.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Card } from "react-bootstrap"
+
+const UpdateStudentPage = () => {
+  return (
+    <div className="d-flex align-items-center justify-content-center">
+      <Card className="w-100" style={{ maxWidth: "500px" }}>
+        <Card.Header>
+          <Card.Title>Update Student</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <p>Update student form will go here...</p>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
+
+export default UpdateStudentPage
